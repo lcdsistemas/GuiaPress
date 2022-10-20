1d2c2c9e1d6d4c745a96da2666a8c55f23281fa3
lcdsistemas <adm@lcdsistemas.com.br>
2022-10-20 15:50:43 -0300

    quarto

--- database/database.js
+++ database/database.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 
 const connection = new Sequelize('GUIAPRESS','root','',{
     host: 'localhost',  //localhost
-    dialect: 'mssql',
+    dialect: 'mysql',
     logging: false,
     timezone: "-03:00" // gravar o horario de criação e update com base na time_zone(google)
 });
