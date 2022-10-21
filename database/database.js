75a1113b4558b1f34ad60d468b22f4313349ba5b
lcdsistemas <adm@lcdsistemas.com.br>
2022-10-21 09:15:59 -0300

    setim

--- database/database.js
+++ database/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize('banco','root','',{
-    host: 'localhost',  //localhost
-    dialect: 'myssql',  // mssql
+const connection = new Sequelize('banco','lcd','LCd358',{
+    host: 'ftp.lcdsistemas.com.br',  //localhost
+    dialect: 'mssql',
     logging: false,
     timezone: "-03:00" // gravar o horario de criação e update com base na time_zone(google)
 });
